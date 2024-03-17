3ec15d47a5822186dc930de147c5fc63790431f1
Noel George C J <noelgeorge2004@gmail.com>
2024-03-17 23:28:28 +0530

    progress save

--- opengl/src/Source.cpp
+++ opengl/src/Source.cpp
@@ -26,7 +26,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.0f);
+        glVertex2f(-0.5f, -0.5f);
         glVertex2f( 0.0f,  0.5f);
         glVertex2f( 0.5f, -0.5f);
         glEnd();
