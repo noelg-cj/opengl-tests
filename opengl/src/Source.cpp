29013b61181fdfbb794e37448519aae1d8519373
Noel George C J <noelgeorge2004@gmail.com>
2024-04-01 22:57:18 +0530

    test

--- opengl/src/Source.cpp
+++ opengl/src/Source.cpp
@@ -12,7 +12,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello, testing", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
