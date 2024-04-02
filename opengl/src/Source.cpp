bbdb9eda1d9d9e3faa08e4313bf67ed726fd8d34
Noel George C J <noelgeorge2004@gmail.com>
2024-04-02 23:39:53 +0530

    drawing arrys

--- opengl/src/Source.cpp
+++ opengl/src/Source.cpp
@@ -44,11 +44,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
