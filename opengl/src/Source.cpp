6bc50292e883fde504b929c86afbccdeeedc1261
Noel George C J <noelgeorge2004@gmail.com>
2024-04-05 00:06:23 +0530

    Error handling for Compile shader

--- opengl/src/Source.cpp
+++ opengl/src/Source.cpp
@@ -8,6 +8,20 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
     const char* src = source.c_str();
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
+    
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
 
     return id;
 }
