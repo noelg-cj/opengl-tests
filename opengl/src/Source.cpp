5504d331bf63f5baaa0bf2a437c4073621d194dd
Noel George C J <noelgeorge2004@gmail.com>
2024-04-04 23:57:57 +0530

    compileShader and createShader

--- opengl/src/Source.cpp
+++ opengl/src/Source.cpp
@@ -6,11 +6,26 @@
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
     unsigned int id = glCreateShader(GL_VERTEX_SHADER);
     const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    return id;
 }
 
-static int createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader) {
     unsigned int program = glCreateProgram();
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 
 int main(void)
